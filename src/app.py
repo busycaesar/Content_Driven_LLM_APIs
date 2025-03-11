101a10a8de47d2d4deb6c59b02891f42df92f282
Dev <busycaesar@gmail.com>
2025-03-11 16:15:03 -0400

    Refactor: Add domain for the CORS.

--- src/app.py
+++ src/app.py
@@ -7,7 +7,10 @@ from routes import routes
 
 # Initiate a Flask application.
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
+CORS(app, resources={r"/*": {"origins": [\
+    "http://localhost:3000",
+    "https://www.shahtech.info"
+    ]}})
 
 # Register the blueprint of all the routes.
 # API calls for the assigned url prefix will be redirected to the routes.
