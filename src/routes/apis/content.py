8dab16066c473734063a13dac4f7d3c2c383c053
Dev <busycaesar@gmail.com>
2025-03-17 09:49:48 -0400

    Refactor(routes): Call functions of controller

--- src/routes/apis/content.py
+++ src/routes/apis/content.py
@@ -1,39 +1,67 @@
 from flask import Blueprint, request, jsonify
 from routes.response import response
+from controller.content import store_new_content, get_all_content, get_stored_content, update_stored_content, delete_stored_content
 
 content_apis = Blueprint("content_apis", __name__)
 
 @content_apis.route("/", methods=["POST"])
 async def post():
     try:
-        return jsonify(response(True, "New content stored.")), 201
+        # Get user id from jwt token.
+        user_id = 1
+        data = request.get_json()
+        content = data.get("content")
+
+        collection_id = await store_new_content(user_id, content)
+
+        return jsonify(response(True, "New content is stored.", collection_id)), 201
     except Exception as e:
         return jsonify(response(False, f"Error: {str(e)}")), 500
 
 @content_apis.route("/", methods=["GET"])
 async def get():
     try:
-        return jsonify(response(True, "New content stored.")), 201
+        # Get user id from jwt token.
+        user_id = 1
+        content = await get_all_content(user_id)
+
+        return jsonify(response(True, "All the content sent.", content)), 200
     except Exception as e:
         return jsonify(response(False, f"Error: {str(e)}")), 500
 
-@content_apis.route("/:contentId", methods=["GET"])
-async def get_content_id():
+@content_apis.route("/<content_id>", methods=["GET"])
+async def get_content_id(content_id):
     try:
-        return jsonify(response(True, "New content stored.")), 201
+        # Get user id from jwt token.
+        user_id = 1
+        content = await get_stored_content(user_id, content_id)
+
+        return jsonify(response(True, "Content sent.", content)), 200
     except Exception as e:
         return jsonify(response(False, f"Error: {str(e)}")), 500
     
-@content_apis.route("/:contentId", methods=["PUT"])
-async def put_content_id():
+@content_apis.route("/<content_id>", methods=["PUT"])
+async def put_content_id(content_id):
     try:
-        return jsonify(response(True, "New content stored.")), 201
+        # Get user id from jwt token.
+        user_id = 1
+        data = request.get_json()
+        content = data.get("content")
+        
+        await update_stored_content(user_id, content_id, content)
+
+        return jsonify(response(True, "Content Updated.")), 201
     except Exception as e:
         return jsonify(response(False, f"Error: {str(e)}")), 500
     
-@content_apis.route("/:contentId", methods=["DELETE"])
-async def delete_content_id():
+@content_apis.route("/<content_id>", methods=["DELETE"])
+async def delete_content_id(content_id):
     try:
-        return jsonify(response(True, "New content stored.")), 201
+        # Get user id from jwt token.
+        user_id = 1
+
+        await delete_stored_content(user_id, content_id)
+
+        return jsonify(response(True, "Delete stored content.")), 201
     except Exception as e:
         return jsonify(response(False, f"Error: {str(e)}")), 500
--- src/routes/apis/llm.py
+++ src/routes/apis/llm.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, request, jsonify
 from routes.response import response
+from controller.llm import get_available_llms
 
 llm_apis = Blueprint("llm_apis", __name__)
 
 @llm_apis.route("/", methods=["GET"])
 async def get():
     try:
-        return jsonify(response(True, "New content stored.")), 201
+        available_llms = await get_available_llms()
+
+        return jsonify(response(True, "Available LLMs sent.", available_llms)), 200
     except Exception as e:
         return jsonify(response(False, f"Error: {str(e)}")), 500
