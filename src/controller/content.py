e28af08bd8af35c659815ff5f8e2ec637e22003d
Dev <busycaesar@gmail.com>
2025-03-17 09:48:57 -0400

    Add: Controller

---
+++ src/controller/content.py
@@ -0,0 +1,14 @@
+async def store_new_content(user_id, content):
+    return True
+
+async def get_all_content(user_id):
+    return True
+
+async def get_stored_content(user_id, content_id):
+    return True
+
+async def update_stored_content(user_id, content_id, content):
+    return True
+
+async def delete_stored_content(user_id, content_id):
+    return True
