a3fb1efecad5c2eef4845611f90cf6b3a502e026
Dev <busycaesar@gmail.com>
2025-03-19 14:06:28 -0400

    Add(src/controller): Add function store_new_content

--- src/controller/content.py
+++ src/controller/content.py
@@ -1,5 +1,13 @@
+from service.content import add_content
+
 async def store_new_content(user_id, content):
-    return True
+    # Make sure that the user id and content is present.
+    if not user_id or not content:
+        raise ValueError("Both user id and content must be provided. Please check the requirements of this API.")
+    
+    collection_id = await add_content(user_id, content)
+
+    return collection_id
 
 async def get_all_content(user_id):
     return True
