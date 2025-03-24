26861266d0ea6ba89f6e209d29ae962bb7707d3f
Dev <busycaesar@gmail.com>
2025-03-24 15:18:48 -0400

    Add(adaptors): Database Adaptor

---
+++ src/adaptors/db_adaptors.py
@@ -0,0 +1,24 @@
+from utils import ErrorMessages
+from flask_sqlalchemy import SQLAlchemy
+
+class DBAdaptor:
+    def __init__(self, connection_string):
+        if not connection_string:
+            raise ValueError(ErrorMessages.MISSING_DATA(
+                ["connection_string"],
+                "Adaptor level error."
+            ))
+        
+        self.connection_string = connection_string
+        self.db = SQLAlchemy()
+
+    def config_app(self, app):
+        app.config["SQLALCHEMY_DATABASE_URI"] = self.connection_string
+
+        self.db.init_app(app)
+
+        with app.app_context():
+            self.db.create_all()
+
+    def __getattr__(self, name):
+        return getattr(self.db, name)
