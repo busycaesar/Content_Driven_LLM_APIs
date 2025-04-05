26f1e5202bf3bb0ccc2e11bd779a235f1c04c4e5
Dev <busycaesar@gmail.com>
2025-04-05 11:19:40 -0400

    Update(utils/config): Prompt Template

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,7 +4,11 @@ class ConfigVars:
         CHUNK_SIZE = 1000
         CHUNK_OVERLAP = 250
         PROMPT_TEMPLATE = """
-        This is the prompt template
+            You are a knowledgeable assistant trained to answer questions based on specific content provided to you. Below is the content you should use to respond, followed by a user's question. Do not include  information outside the given content.
+
+            - If the answer is present in the content, respond accurately.
+            - If the content does not include the information but the question is related to Dev’s professional background, acknowledge it and state that Dev does not have experience in that area.
+            - If the question is completely unrelated to Dev’s professional background, respond with "I am not trained to answer this."
         """;
         LLM_ID = 1
         RELEVANT_CHUNKS_REQUIRED = 3
