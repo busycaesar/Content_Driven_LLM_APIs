bbe3ddbfac1df3af00a08fc95d8a84914d241aea
Dev <busycaesar@gmail.com>
2025-04-09 16:54:10 -0400

    Add(frontend): Button to add new content

--- frontend/src/app/content/page.jsx
+++ frontend/src/app/content/page.jsx
@@ -2,13 +2,19 @@
 
 import React from "react";
 import { useContentListController } from "@/controller";
+import { Button } from "@/components/ui/button";
 
 export default function Content() {
   const contents = useContentListController();
 
-  return contents?.length > 0 ? (
-    contents?.map((content) => <div>{content}</div>)
-  ) : (
-    <>No Content Stored.</>
+  return (
+    <>
+      <Button className="my-2">Add Content</Button>
+      {contents?.length > 0 ? (
+        contents?.map((content) => <div>{content}</div>)
+      ) : (
+        <>No Content Stored.</>
+      )}
+    </>
   );
 }
