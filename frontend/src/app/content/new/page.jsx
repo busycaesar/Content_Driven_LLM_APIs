3cc0e5018a8ca11333b4a9c6dd04cddde31b7158
Dev <busycaesar@gmail.com>
2025-04-09 16:58:02 -0400

    Install(frontend): ShadCN Textarea

--- frontend/src/app/content/new/page.jsx
+++ frontend/src/app/content/new/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
 
 export default function newContent() {
   return (
     <>
       <h1>New Content</h1>
+      <Input type="text" />
     </>
   );
 }
